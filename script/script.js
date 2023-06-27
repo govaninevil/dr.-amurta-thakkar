35154c24fadb2d3f45c4a04b746a9f9d08d5e7cd
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2023-06-27 19:13:54 +0530

    main

--- script/script.js
+++ script/script.js
@@ -4,5 +4,12 @@ $(document).on('ready', function () {
         $("#menu-bar").toggleClass("show");
     });
 
-
+    $('.collage.body h3').each(function() {
+        var tis = $(this), state = false, answer = tis.next('div').slideUp();
+        tis.click(function() {
+            state = !state;
+            answer.slideToggle(state);
+            tis.toggleClass('active',state);
+        });
+    });
 });
