51d22d850250e9eb6f0cc6a6b70ac70c334fec4d
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2023-07-27 17:24:45 +0530

    main

--- script/script.js
+++ script/script.js
@@ -1,7 +1,4 @@
 $(document).on('ready', function () {
-
-    
-
     $("#menu-button").on("click", function () {
         $("#menu-bar").toggleClass("show");
     });
@@ -10,13 +7,22 @@ $(document).on('ready', function () {
         $("body").toggleClass("mobile");
     });
 
-    $('.collage.body h3').each(function() {
+    $('.collage.body h3').each(function () {
         var tis = $(this), state = false, answer = tis.next('div').slideUp();
-        tis.click(function() {
+        tis.click(function () {
             state = !state;
             answer.slideToggle(state);
-            tis.toggleClass('active',state);
+            tis.toggleClass('active', state);
         });
+    });
+
+    $(".reg-a").on("click", function () {
+        $(".pop-a").toggleClass("open");
+        $("body").toggleClass("back");
+    });
+
+    $("#close").on("click", function () {
+        $(".pop-a").removeClass("open");
     });
 
     $('#parentVerticalTab').easyResponsiveTabs({
@@ -24,16 +30,16 @@ $(document).on('ready', function () {
         width: 'auto', //auto or any width like 600px
         fit: true, // 100% fit in a container
         tabidentify: 'hor_1', // The tab groups identifier
-        activate: function(event) { // Callback function if tab is switched
+        activate: function (event) { // Callback function if tab is switched
             var $tab = $(this);
             var $info = $('#nested-tabInfo');
             var $name = $('span', $info);
             $name.text($tab.text());
             $info.show();
         }
-    }); 
+    });
 
-    
+
     $(window).scroll(function () {
         if ($(this).scrollTop() > 150) {
             $('header').addClass("small");
@@ -42,19 +48,19 @@ $(document).on('ready', function () {
         }
     });
 
-    
-    
-    $('#certificate-slider').slick({
-            dots: false,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 1,
-            adaptiveHeight: true
-    }); 
 
-     // tabbing
-    
+
+    $('#certificate-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+
+    // tabbing
+
 });
 $(document).on('ready', function () {
-   
+
 });
